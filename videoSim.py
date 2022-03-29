ca6723b7c433338cb3b6b3571f37eee2783d9d90
hesham moneer <hesham.moneer@outlook.com>
2022-03-29 21:25:40 +0200

    added actual FPS realtime display in stdout

--- videoSim.py
+++ videoSim.py
@@ -6,21 +6,18 @@ def vSim(cap, width = 32, height = 32, noColors = 16):
     # get the original video FPS
     fps = cap.get(cv2.CAP_PROP_FPS)
     print("Original FPS: "+str(fps))
-    noFrames = 0
-    elapsedTime = 0
     while True:
-        startTime = time.time()
         ret,frame = cap.read()
         if(not ret): break
-        noFrames += 1
         cv2.imshow('Original', frame)
+        startTime = time.time()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = pSim(frame, width, height, noColors)
+        endTime = time.time()
+        print('Actual FPS: '+ str(int(1/(endTime-startTime))), end='\r')
         cv2.imshow('Phosphenated', frame)
-        elapsedTime += time.time() - startTime
         if cv2.waitKey(1) & 0xFF == ord('0'): break
 
-    print('Actual FPS: '+ str(noFrames/elapsedTime))
     cap.release()
     cv2.destroyAllWindows() # destroy all opened windows
 
