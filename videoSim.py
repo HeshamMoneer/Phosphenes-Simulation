c25076aa21ba1d3b57232bdcbc39199f6965aaf9
hesham moneer <hesham.moneer@outlook.com>
2022-03-29 02:54:28 +0200

    added video FPS calculation with phosphene simulation for benchmarking

--- videoSim.py
+++ videoSim.py
@@ -1,14 +1,21 @@
 import cv2
 from phosphenesSim import pSim
+import time
 
 cap = cv2.VideoCapture('./videos/vid2.mp4')
+noFrames = 0
+start = time.time()
 while cap.isOpened():
     ret,frame = cap.read()
+    if(not ret): break
+    noFrames += 1
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = pSim(frame)
-    cv2.imshow('window-name', frame)
-    if cv2.waitKey(30) & 0xFF == ord('0'):
+    cv2.imshow('Phosphenated', frame)
+    if cv2.waitKey(1) & 0xFF == ord('0'):
         break
+end = time.time()
 
+print('FPS = '+ str(noFrames/(end-start)))
 cap.release()
 cv2.destroyAllWindows() # destroy all opened windows
