dc1bead2f0179b1cf5200f4cc48deefaa1547de8
hesham moneer <hesham.moneer@outlook.com>
2022-04-09 15:47:53 +0200

    added the eyes classifier

--- facesDetector.py
+++ facesDetector.py
@@ -1,26 +1,41 @@
 import cv2
 import numpy as np
 
-def detectAllFaces(img, classifier):
-  bboxes = classifier.detectMultiScale(img)
+def detectAllFaces(img, classifiers):
+  bboxes = classifiers[0].detectMultiScale(img)
   for box in bboxes:
       x, y, width, height = box
       cv2.rectangle(img, (x, y), (x + width, y + height), 255, 1)
   return img
 
-def scaleToFirstFace(img, classifier):
-  bboxes = classifier.detectMultiScale(img)
+def scaleToFirstFace(img, classifiers):
+  bboxes = classifiers[0].detectMultiScale(img)
   x, y, width, height = bboxes[0] if len(bboxes) > 0 else (0,0,img.shape[1],img.shape[0])
   return img[y:y+height, x:x+width]
 
-def brightenFirstFace(img, classifier):
-  bboxes = classifier.detectMultiScale(img)
+def brightenFirstFace(img, classifiers):
+  bboxes = classifiers[0].detectMultiScale(img)
   if len(bboxes) > 0:
     x, y, width, height = bboxes[0]
     it = np.nditer(img[y:y+height, x:x+width], op_flags=['readwrite'])
     while not it.finished:
       it[0] = it[0] + 30 if it[0] < 225 else 255
       it.iternext()
+  return img
+
+def detectFacesWithEyes(img, classifiers):
+  face_detector = classifiers[0]
+  eyes_detector = classifiers[1]
+
+  faces = face_detector.detectMultiScale(img)
+
+  for x,y, width, height in faces:
+    cv2.rectangle(img, (x, y), (x + width, y + height), 255, 1)
+    subImg = img[y:y+height, x:x+width]
+    eyes = eyes_detector.detectMultiScale(subImg)
+    for x2,y2, width2, height2 in eyes:
+      cv2.rectangle(img, (x + x2, y + y2), (x + x2 + width2, y + y2 +height2), 255, 1)
+  
   return img
 
   
--- videoSim.py
+++ videoSim.py
@@ -1,6 +1,6 @@
 import cv2
 from phosphenesSim import (pSim, Simode)
-from facesDetector import (detectAllFaces, scaleToFirstFace, brightenFirstFace)
+from facesDetector import (detectAllFaces, scaleToFirstFace, brightenFirstFace, detectFacesWithEyes)
 from fps import (printFPS, printOrginialFPS)
 from gaussArray import gaussArr
 import enum
@@ -10,6 +10,7 @@ class Modes(enum.Enum): # Face Detection mode
     DETECT_ALL_FACES = 1
     SCALE_TO_FIRST_FACE = 2
     BRIGHTEN_FIRST_FACE = 3
+    DETECT_FACES_WITH_EYES = 4
 
 def vSim(cap, dim = 32, dimWin = 640, mLevels = 16, simode = Simode.BCM, facesMode = Modes.NOTHING):
     # Computer the gauss array in case needed
@@ -20,31 +21,36 @@ def vSim(cap, dim = 32, dimWin = 640, mLevels = 16, simode = Simode.BCM, facesMo
         gArr = gaussArr(radius)
     
     printOrginialFPS(cap)
-    classifier = cv2.CascadeClassifier('classifiers/cc.xml')
+    faces_classifier = cv2.CascadeClassifier('classifiers/cc.xml')
+    eyes_classifier = cv2.CascadeClassifier('classifiers/ecc.xml')
+    classifiers = [faces_classifier, eyes_classifier]
     while True:
         ret,frame = cap.read()
         if(not ret): break
-        frame = printFPS(lambda: fSim(frame, classifier, dim, dimWin, mLevels, gArr, simode, facesMode))
+        frame = printFPS(lambda: fSim(frame, classifiers, dim, dimWin, mLevels, gArr, simode, facesMode))
         cv2.imshow('Phosphenated ' + simode.name, frame)
         if cv2.waitKey(1) & 0xFF == ord('0'): break
     cap.release()
     cv2.destroyAllWindows()
 
-def fSim(frame, classifier, dim, dimWin, mLevels, gArr, simode, facesMode):
+def fSim(frame, classifiers, dim, dimWin, mLevels, gArr, simode, facesMode):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     if facesMode == Modes.DETECT_ALL_FACES: 
-        frame = detectAllFaces(frame, classifier)
+        frame = detectAllFaces(frame, classifiers)
     elif facesMode == Modes.SCALE_TO_FIRST_FACE: 
-        frame = scaleToFirstFace(frame, classifier)
+        frame = scaleToFirstFace(frame, classifiers)
     elif facesMode == Modes.BRIGHTEN_FIRST_FACE: 
-        frame = brightenFirstFace(frame, classifier)
+        frame = brightenFirstFace(frame, classifiers)
+    elif facesMode == Modes.DETECT_FACES_WITH_EYES:
+        frame = detectFacesWithEyes(frame, classifiers)
+    return frame
     return pSim(img = frame, dim = dim, dimWin = dimWin, mLevels = mLevels, simode = simode, gArr = gArr)
 
 
 def main():
     vidNumber = eval(input("Enter Video number: "))
     cap = cv2.VideoCapture('./videos/vid' + str(vidNumber) + '.mp4' if vidNumber > 0 else 0) # vidNumber <= 0 opens the webcam
-    vSim(cap, simode = Simode.BCM, facesMode = Modes.DETECT_ALL_FACES)
+    vSim(cap, simode = Simode.BCM, facesMode = Modes.DETECT_FACES_WITH_EYES)
 
 if __name__ == '__main__':
     main()
