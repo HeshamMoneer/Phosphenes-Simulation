4ef8a0b7a66375857417234a7a9010951a394fc3
hesham moneer <hesham.moneer@outlook.com>
2022-04-22 22:42:35 +0200

    removed unnecessary fps file

--- videoSim.py
+++ videoSim.py
@@ -1,11 +1,12 @@
 import cv2
+import dlib
+import time
+
 from enums import (Modes, Simode)
 from phosphenesSim import (pSim, Simode)
 from bboxes import (updateBBoxes, applyBBoxes)
-from fps import (printFPS, printOrginialFPS)
 from gaussArray import gaussArr
 from cropper import squareCrop
-import dlib
 
 
 def vSim(cap, dim = 32, dimWin = 640, mLevels = 16, simode = Simode.BSM, facesMode = Modes.NOTHING, cache = {}):
@@ -16,7 +17,9 @@ def vSim(cap, dim = 32, dimWin = 640, mLevels = 16, simode = Simode.BSM, facesMo
         radius = int(squareSide * 0.7)
         gArr = gaussArr(radius)
     
-    printOrginialFPS(cap)
+    fps = cap.get(cv2.CAP_PROP_FPS) # get the original video FPS
+    print("Original FPS: "+str(fps))
+
     faces_classifier = cv2.CascadeClassifier('classifiers/cc.xml')
     eyes_classifier = cv2.CascadeClassifier('classifiers/ecc.xml')
     detector = dlib.get_frontal_face_detector()
@@ -27,12 +30,19 @@ def vSim(cap, dim = 32, dimWin = 640, mLevels = 16, simode = Simode.BSM, facesMo
     while True:
         ret,frame = cap.read()
         if(not ret): break
+
+        startTime = time.time()
+        
         frame = squareCrop(frame)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         bboxes, counter = updateBBoxes(frame, bboxes, counter, classifiers, facesMode)
         frame = applyBBoxes(frame, bboxes, facesMode)
         frame, cache = pSim(img = frame, dim = dim, dimWin = dimWin, mLevels = mLevels, simode = simode, gArr = gArr, cache = cache)
         cv2.imshow('Phosphenated ' + simode.name, frame)
+        
+        endTime = time.time()
+        print('FPS: '+ str(int(1/(endTime-startTime))), end='\r')
+
         if cv2.waitKey(1) & 0xFF == ord('0'): break
     cap.release()
     cv2.destroyAllWindows()
