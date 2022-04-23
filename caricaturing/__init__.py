8a79ea9bc7c48eb843ed6715b079b6db2d30155f
hesham moneer <hesham.moneer@outlook.com>
2022-04-23 03:59:57 +0200

    changed caricaturing architecture to get ready for integration with vSim

--- caricaturing/__init__.py
+++ caricaturing/__init__.py
@@ -1,90 +1,39 @@
-from face_landmark_detection import (generate_face_correspondences, generate_face_land_marks)
-from delaunay_triangulation import make_delaunay
+import cv2
+
+import caric_config as cc
+from face_landmark_detection import generate_face_correspondences
 from face_morph import generate_morph_frame
 
-import cv2
-import dlib
+def doMorphing(faceImg):
+	[faceImg, faceP] = generate_face_correspondences(faceImg, cc.avgS, cc.predictor)
+	return generate_morph_frame(faceImg, faceP, cc.avgP, cc.tri, cc.alpha)
 
-def detect_face(img, face_cascade):
-    if len(img.shape) > 2 :
-      img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(img, minNeighbors = 8)
-    if (len(faces) == 0):
-      return img, None
-    (x, y, w, h) = faces[0]
-    return img[y:y+w, x:x+h], faces[0]
+def caric(frame, faceOnly = True):
+	if not faceOnly: 
+		frame, rect = cc.detect_face(frame)
+	else:	
+		rect = (0,0,frame.shape[1], frame.shape[0])
 
-def doMorphing(faceImg, avgP, avgS, predictor, tri, alpha = 1):
-	[faceImg, faceP] = generate_face_correspondences(faceImg, avgS, predictor)
-	# for point in faceP:
-	# 	cv2.circle(faceImg, point, 1, 255,1)
-	# cv2.rectangle(faceImg, (2,2), (faceImg.shape[1] -2, faceImg.shape[0] -2), 255, 2)
-	# return faceImg
-	# for t in tri:
-	# 	cv2.line(faceImg, faceP[t[0]],faceP[t[1]], 255, 2)
-	# 	cv2.line(faceImg, faceP[t[2]],faceP[t[1]], 255, 2)
-	# 	cv2.line(faceImg, faceP[t[0]],faceP[t[2]], 255, 2)
-	return generate_morph_frame(faceImg, faceP, avgP, tri, alpha)
+	if rect is not None:
+		caricature = doMorphing(frame)
+		h, w = frame.shape
+		caricature = cv2.resize(caricature, (w,h))
+		caricature[caricature==0] = frame[caricature==0]
+		frame = caricature
+	return frame
 
 def main():
-	face_cascade = cv2.CascadeClassifier('utils/cc.xml')
-	predictor = dlib.shape_predictor('utils/shape_predictor_68_face_landmarks.dat')
-	avg = cv2.imread('images/aligned_images/avgM.png')
-	avg, _ = detect_face(avg, face_cascade)
-	avg = cv2.resize(avg, (160, 160))
-	avgP = generate_face_land_marks(avg, predictor) # average landmarks
-	avgS = avg.shape # average shape
-	tri = make_delaunay(avgS, avgP)
-
+	cc.init()
 	cap = cv2.VideoCapture(0)
-
-	# result = cv2.VideoWriter('caricaturing.avi', 
-  #                        cv2.VideoWriter_fourcc(*'MJPG'),
-  #                        20, (int(cap.get(3)), int(cap.get(4))), 0)
 	while True:
 		ret, frame = cap.read()
 		if(not ret): break
-		face, rect = detect_face(frame, face_cascade)
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		if rect is not None:
-			face = doMorphing(face, avgP, avgS, predictor, tri)
-			# face = cv2.equalizeHist(face)
-			# frame = cv2.equalizeHist(frame)
-			(x,y,w,h) = rect
-			face = cv2.resize(face, (w,h))
-			subframe = frame[y:y+h, x:x+w]
-			face[face==0] = subframe[face==0]
-			frame[y:y+h, x:x+w] = face
-			# frame = face
-		# result.write(frame)
-		cv2.imshow('frame', frame)
+		frame = caric(frame, False)
+		cv2.imshow('caricature', frame)
 		if cv2.waitKey(1) & 0xFF == ord('0'): break
-
 	cap.release()
-	# result.release()
 	cv2.destroyAllWindows()
 
-	# parser = argparse.ArgumentParser()
-	# parser.add_argument("--img1", required=True, help="The First Image")
-	# parser.add_argument("--img2", required=True, help="The Second Image")
-	# args = parser.parse_args()
-
-	# image1 = cv2.imread(args.img1)
-	# image2 = cv2.imread(args.img2)
-
-	# image1 = cv2.resize(image1, (300,300))
-	# image2 = cv2.resize(image2, (300,300))
-
-	# image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-	# image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-
-	# for i in range(11):
-	# 	img = doMorphing(image1, image2, i * 0.1)
-	# 	cv2.imshow('', img)
-	# 	if cv2.waitKey(1) & 0xFF == ord('0'):
-	# 		break
-	# cv2.waitKey(0)
-	# cv2.destroyAllWindows()
 
 if __name__ == "__main__":
 	main()
---
+++ caricaturing/caric_config.py
@@ -0,0 +1,39 @@
+import cv2
+import dlib
+
+from delaunay_triangulation import make_delaunay
+from face_landmark_detection import generate_face_land_marks
+
+'''
+Caricaturing configuration values that could be manipulated
+
+avg(Path): to choose which path is to the average face
+alpha: caricaturing factor; typically 0 <= alpha <= 1
+'''
+
+def init(fc = None, p = None):
+  global face_cascade, predictor, avg, avgP, avgS, tri, alpha
+  if fc: face_cascade = fc
+  else: face_cascade = cv2.CascadeClassifier('utils/cc.xml')
+
+  if p: predictor = p
+  else: predictor = dlib.shape_predictor('utils/shape_predictor_68_face_landmarks.dat')
+
+  avg = cv2.imread('images/aligned_images/avgF.png')
+  avg, _ = detect_face(avg)
+  avg = cv2.resize(avg, (160, 160))
+
+  avgP = generate_face_land_marks(avg, predictor) # average landmarks
+  avgS = avg.shape # average shape
+
+  tri = make_delaunay(avgS, avgP)
+  alpha = 1
+
+def detect_face(img):
+  if len(img.shape) > 2 :
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  faces = face_cascade.detectMultiScale(img, minNeighbors = 8)
+  if (len(faces) == 0):
+    return img, None
+  (x, y, w, h) = faces[0]
+  return img[y:y+w, x:x+h], faces[0]
