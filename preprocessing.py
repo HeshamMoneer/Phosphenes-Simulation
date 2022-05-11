e9bd53e9fddd7091b5f8dccf3b62f28b6ac4f88b
hesham moneer <hesham.moneer@outlook.com>
2022-05-12 00:27:05 +0200

    added square fit function in preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,8 @@ import simConfig as sc
 from modulator import modulate
 
 def prep(img):
-  img = squareCrop(img) # crop image to be a square; if not already a square
+  # img = squareCrop(img) # crop image to be a square; if not already a square
+  img = squareFit(img)
   img = cv2.resize(img, (sc.dim, sc.dim)) # resize image to desired resolution; bilinear interpolation
   img = cv2.equalizeHist(img)
   img = modulate(img, 255, sc.mLevels - 1) # modulate colors to given levels; SIMD applied
@@ -21,3 +22,13 @@ def squareCrop(img):
   sh = int(height/2 - dim /2)
   sw = int(width/2 - dim /2)
   return img[sh:sh+dim, sw:sw+dim]
+
+def squareFit(img):
+  height, width = img.shape[0], img.shape[1]
+  if height == width: return img
+  dim = max([height, width])
+  res = np.zeros((dim, dim), dtype=np.uint8)
+  sh = int((dim - height)/2)
+  sw = int((dim - width)/2)
+  res[sh:height+sh, sw:width+sw] = img
+  return res
